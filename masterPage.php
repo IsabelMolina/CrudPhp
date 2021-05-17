853104ff11dba6d97bd82c60ebefd22dcb58d651
unknown <isabelcmolina12@gmail.com>
2021-05-16 21:18:03 -0500

    correccion nombre carpeta

--- masterPage.php
+++ masterPage.php
@@ -2,7 +2,7 @@
   class masterPage {
 
     function obtenerScriptsCabecera(){
-      $scripts = "<link href='public/css/bootstrap.min.css' rel='stylesheet'>";
+      $scripts = "<link href='assets/css/bootstrap.min.css' rel='stylesheet'>";
       $scripts .= "<link rel='stylesheet' href='https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css'>";
       return $scripts;
     }
@@ -10,7 +10,7 @@
     function obtenerScriptsPie(){
       $scripts = "<script src='https://code.jquery.com/jquery-2.2.4.min.js' integrity='sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=' crossorigin='anonymous'></script>";
       $scripts .= "<script type='text/javascript' src='https://cdn.datatables.net/v/dt/dt-1.10.24/datatables.min.js'></script>";
-      $scripts .= "<script src='public/js/bootstrap.min.js' ></script>";
+      $scripts .= "<script src='assets/js/bootstrap.min.js' ></script>";
       return $scripts;
     }
 
